b93c2382d835ad81ac5055574cd70f46c38c77cc
Burak Bayrakdar <burak.bayrakdar@rakuten.com>
2018-05-18 12:55:29 +0900

    publisher bean made optional for non-listener mappers

--- src/main/java/com/buraequete/orikautomation/config/MapperConfiguration.java
+++ src/main/java/com/buraequete/orikautomation/config/MapperConfiguration.java
@@ -2,6 +2,7 @@ package com.buraequete.orikautomation.config;
 
 import com.buraequete.orikautomation.events.MappingEventPublisher;
 import com.buraequete.orikautomation.mapper.BeanMapper;
+import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfiguration {
 
 	@Bean
-	public BeanMapper beanMapper(MappingEventPublisher mappingEventPublisher) {
-		return new BeanMapper(mappingEventPublisher);
+	public BeanMapper beanMapper(Optional<MappingEventPublisher> mappingEventPublisher) {
+		return mappingEventPublisher.map(BeanMapper::new).orElseGet(BeanMapper::new);
 	}
 }
